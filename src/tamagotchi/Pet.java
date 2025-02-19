cae8cc0131d6b565bbd1f530cc4295293e8eea2b
Isla Purkey <purkeyr@wit.edu>
2025-02-19 12:45:27 -0500

    Created Java project

---
+++ src/tamagotchi/Pet.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package tamagotchi;
+
+/**
+ * 
+ */
+public class Pet extends PetStats {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/tamagotchi/Tic_Tac_Toe_Game.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package tamagotchi;
+
+/**
+ * 
+ */
+public class Tic_Tac_Toe_Game {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
