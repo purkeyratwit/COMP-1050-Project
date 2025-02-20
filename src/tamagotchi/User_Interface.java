d30d2f455379103415b74bf6789edac15341e404
Isla Purkey <purkeyr@wit.edu>
2025-02-19 19:35:09 -0500

    Added empty methods and JavaDoc documentation (User_Interface)

--- src/tamagotchi/User_Interface.java
+++ src/tamagotchi/User_Interface.java
@@ -1,19 +1,81 @@
-/**
- * 
- */
 package tamagotchi;
 
 /**
+ * Class that prints information to the console based on the user's input and/or
+ * current state of the program
  * 
+ * @author Isla Purkey
  */
 public class User_Interface {
 
+	String formattedString = "";
+
 	/**
-	 * @param args
+	 * Constructor, takes the current pet as input and creates a base for future
+	 * method calls to be based off of
+	 * 
+	 * @param pet The current pet
 	 */
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	public User_Interface(Pet pet) {
+		// TODO
 	}
-
+	
+	/**
+	 * Updates the current console output to represent the current food class
+	 * 
+	 * @param foodInput The current state of the food class
+	 */
+	public void updateScreen(Food foodInput) {
+		// TODO
+	}
+	
+	/**
+	 * Updates the current console output to represent the current dirtiness class
+	 * 
+	 * @param dirtinessInput The current state of the dirtiness class
+	 */
+	public void updateScreen(Dirtiness dirtinessInput) {
+		// TODO
+	}
+	
+	/**
+	 * Updates the current console output to represent the current boredom class
+	 * 
+	 * @param boredomInput The current state of the boredom class
+	 */
+	public void updateScreen(Boredom boredomInput, int questionNumber) {
+		// TODO
+	}
+	
+	/**
+	 * Updates the current console output to represent the current pet's health
+	 * 
+	 * @param stats true if the pet is healthy, false otherwise
+	 */
+	public void updateScreen(boolean stats) {
+		// TODO
+	}
+	
+	/**
+	 * Updates the current console output to represent the main menu
+	 */
+	public void updateScreen() {
+		// TODO
+	}
+	
+	/**
+	 * Clears the console output to allow for new text to be printed
+	 */
+	public void clearScreen() {
+		// TODO
+	}
+	
+	/**
+	 * Prints the current options available to the user 
+	 * 
+	 * @param optionsList Array of available options
+	 */
+	public void printOptions(String[] optionsList) {
+		// TODO
+	}
 }
